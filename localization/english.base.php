b15f24c2e891c72f0435eda8b11be63ab30cea9c
Nepgearsy <timertimer5@gmail.com>
2018-09-06 05:11:35 +0200

    added some lines

--- localization/english.base.php
+++ localization/english.base.php
@@ -1,4 +1,9 @@
 <?php
+    /*
+    $l['english'][''] = '';
+    Localization::text("");
+    */
+
     $l = array();
     $l['english'] = array();
     $l['english']['mws_error_tabtitle'] = "modworkshop | Error";
@@ -14,8 +19,20 @@
     $l['english']['mws_usergroup_guest'] = "Guest";
     $l['english']['mws_usergroup_member'] = "Member";
 
+    $l['english']['mws_error'] = 'Error';
+
     $l['english']['mws_cookie_block_l1'] = "We use cookies, they are small files that has information stored on your computer.";
     $l['english']['mws_cookie_block_l2'] = "Do you accept cookies?";
     $l['english']['mws_cookie_block_l3'] = '<b style="color: red;">NOTE:</b> To be able to login on our website, you must accept cookies first.';
     $l['english']['mws_cookie_accept'] = 'Accept';
     $l['english']['mws_cookie_learnmore'] = 'Learn More';
+
+    $l['english']['mws_profile_error_user_doesnt_exist'] = 'This user does not exist.';
+    $l['english']['mws_profile_error_no_user_alert'] = 'You cannot show this page without any user specified.';
+    $l['english'][''] = '';
+    $l['english'][''] = '';
+    $l['english'][''] = '';
+
+
+    $l['english']['mws_search_error_no_argument'] = 'What about you give us hints on what you want to search? It\'s the bar above...';
+    $l['english']['mws_search_error_no_argument_ntf'] = 'You cannot search without any arguments.';
