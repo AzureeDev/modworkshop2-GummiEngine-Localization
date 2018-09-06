b00457dc3e4596e55228dc355e23676a154a3ace
Nepgearsy <timertimer5@gmail.com>
2018-09-06 04:55:33 +0200

    1

---
+++ localization/english.base.php
@@ -0,0 +1,21 @@
+<?php
+    $l = array();
+    $l['english'] = array();
+    $l['english']['mws_error_tabtitle'] = "modworkshop | Error";
+    $l['english']['mws_index_last_announcement_title'] = "Last announcement";
+    $l['english']['mws_index_discord_title'] = "Discord";
+    $l['english']['mws_index_new_updated_subs_title'] = "New & Updated submissions";
+    $l['english']['mws_index_popular_subs_title'] = "Popular submissions the past 7 days";
+
+    $l['english']['mws_mods_error_doesnt_exist'] = "Error. This mod doesn't exist.";
+    $l['english']['mws_mods_tabtitle_mod_doesnt_exist'] = "modworkshop | Unknown mod";
+    $l['english']['mws_mods_no_description_default'] = "No description provided.";
+
+    $l['english']['mws_usergroup_guest'] = "Guest";
+    $l['english']['mws_usergroup_member'] = "Member";
+
+    $l['english']['mws_cookie_block_l1'] = "We use cookies, they are small files that has information stored on your computer.";
+    $l['english']['mws_cookie_block_l2'] = "Do you accept cookies?";
+    $l['english']['mws_cookie_block_l3'] = '<b style="color: red;">NOTE:</b> To be able to login on our website, you must accept cookies first.';
+    $l['english']['mws_cookie_accept'] = 'Accept';
+    $l['english']['mws_cookie_learnmore'] = 'Learn More';
